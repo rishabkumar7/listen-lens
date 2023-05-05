6bee59ab960d13f49fd9a729c93ac4f895a9fb29
Rishab Kumar <rishabkumar7@gmail.com>
2023-05-05 14:05:02 -0400

    Added GA to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 // pages/_app.js
 import '../styles.css';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
+  <GoogleAnalytics trackPageViews />
+  
   return <Component {...pageProps} />;
 }
 
