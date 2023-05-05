7329aef9ebc604e2b386e76b73f431e679654a63
Rishab Kumar <rishabkumar7@gmail.com>
2023-05-05 14:37:41 -0400

    Added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 // pages/_app.js
 import '../styles.css';
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   <GoogleAnalytics trackPageViews />
   
-  return <Component {...pageProps} />;
+  return (
+    <>
+  <Component {...pageProps} />
+  <Analytics />
+  </>
+  );
 }
 
 export default MyApp;
